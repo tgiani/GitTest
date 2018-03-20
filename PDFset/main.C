d26f998d960b2410e4a75d892a009f1133e6d19e
tgiani <s1792848@staffmail.ed.ac.uk>
2018-03-20 17:51:06 +0000

    attempt

--- PDFset/main.C
+++ PDFset/main.C
@@ -7,65 +7,66 @@ using namespace std;
 using namespace APFEL;
 using namespace LHAPDF;
 
+LHAPDF::PDF * p_pdf;
+double mb = 4.5;
+double Q0 = 2;
 
-double Q0=2;
-double mb=4.5;
 
-double xpdf(double x, double Q, int flavour){
 
-  if(flavour==5 || flavour==-5)
-          
-      if(Q > mb){
 
-       double Qa(mb), Qb(Q), eps = 1e-10;    
+  extern "C" void externalsetapfel_(const double& x, const double& Q, double *xf)
+{
+  
+  for (int i=0; i<13; ++i){
+    xf[i] = (double) p_pdf->xfxQ(i-6,x,Q);    // light flavour and charm PDFs are those of the input set, which for that flavours has NNPDF31
+  }
+
+  // for the bottom quark, if Q<mb we take NNPDF31_bintrinsic, if Q>mb we evolve NNPDF31_bintrinsic in 5FS
+  double bpdf(0.);
+  if(Q > mb){
+
+       double Qa(mb), Qb(Q), eps = 1e-10;
        APFEL::SetPerturbativeOrder(2);
-       APFEL::SetFFNS(5);
        APFEL::SetPDFSet("NNPDF31_bintrinsic_NLL.LHgrid");
-       APFEL::InitializeAPFEL();
+       APFEL::InitializeAPFEL(); 
        APFEL::EvolveAPFEL(Q0,Qa);
-       return APFEL::xPDF(flavour,x);
+       bpdf = APFEL::xPDF(5,x);
    
       }
 
       if(Q <= mb){
 
-       LHAPDF::PDF * below_mb_pdf = LHAPDF::mkPDF("NNPDF31_bintrinsic_NLL", 0); //pdf to use for Q<m
-       return(below_mb_pdf->xfxQ(flavour,x,Q));
+       bpdf = (double) p_pdf->xfxQ(5,x,Q);;
        
-      } 
-
-
-  else{
-
-      if(Q > Q0){ 
-
-       double Qa(mb), Qb(Q), eps = 1e-10;    
-       APFEL::SetPerturbativeOrder(2);
-       APFEL::SetVFNS;
-       APFEL::SetPDFSet("NNPDF31_bintrinsic_NLL.LHgrid");
-       APFEL::InitializeAPFEL();
-       APFEL::EvolveAPFEL(Q0,Qa);
-       return APFEL::xPDF(flavour,x);
-   
-      }
-
-      if(Q <= Q0){
-
-       LHAPDF::PDF * below_mb_pdf = LHAPDF::mkPDF("NNPDF31_bintrinsic_NLL", 0); 
-       return(below_mb_pdf->xfxQ(flavour,x,Q));
-       
-      } 
-            
-  }
-
+} 
+  xf[1] = xf[11] = bpdf;
 }
 
 
-  
-  
 
-int main(){
+int main()
+{
 
+  APFEL::SetTheory("QCD");
+  APFEL::SetPerturbativeOrder(2);
+  APFEL::SetPDFSet("external");  
+  APFEL::SetMaxFlavourAlpha(5);
+  APFEL::SetMaxFlavourPDFs(5);
+  APFEL::SetVFNS();                // apfel evolution will be used only when Q>mb for bpdf, therefore only with 5 active flavours
 
- return 0;
+  p_pdf = LHAPDF::mkPDF("NNPDF31_nnlo_as_0118", 0);
+  APFEL::InitializeAPFEL();
+
+  APFEL::SetPoleMasses(p_pdf->quarkMass(4),
+		       p_pdf->quarkMass(5),
+		       p_pdf->quarkMass(6));
+
+  APFEL::SetAlphaQCDRef(p_pdf->alphasQ(91.1987),91.1876);
+  double Qin(-1.);
+  //APFEL::InitializeAPFEL();
+  APFEL::SetLHgridParameters(100,50,1e-9,1e-1,1,50,Qin*Qin,1e10);
+  APFEL::LHAPDFgrid(0,Qin,"NNPDF31_test_NLL");
+ 
+  return 0;
+
 }
